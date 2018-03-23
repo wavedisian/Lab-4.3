7f97db046bbb94d2c184eca5c739f765fbb69002
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2018-03-23 09:22:07 -0400

    wq

--- Lab 4.3/src/defaultPackage/GameCode.java
+++ Lab 4.3/src/defaultPackage/GameCode.java
@@ -1,5 +1,13 @@
 package defaultPackage;
 
-public class GameCode {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GameCode extends Application{
 
 }
