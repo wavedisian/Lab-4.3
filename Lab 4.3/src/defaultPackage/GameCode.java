a0f022c9549791a93911a7eeeb27fc12bdb03faf
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-03-27 09:14:12 -0400

    Meeep Booop Beeep

--- Lab 4.3/src/defaultPackage/GameCode.java
+++ Lab 4.3/src/defaultPackage/GameCode.java
@@ -13,7 +13,7 @@ public class GameCode extends Application
 	@Override
 	public void start(Stage primaryStage) 
 	{
-		//Red Button
+//Red Button
 		 primaryStage.setTitle("Red");
 	        Button btn = new Button();
 	        btn.setText("Say 'Red'");
@@ -29,7 +29,7 @@ public class GameCode extends Application
 	        root.getChildren().add(btn);
 	        primaryStage.setScene(new Scene(root, 300, 250));
 	        primaryStage.show();
-	   	 //Blue Button
+//Blue Button
 	      primaryStage.setTitle("Blue");
 	        Button btn1 = new Button();
 	        btn.setText("Say 'Blue'");
@@ -44,6 +44,39 @@ public class GameCode extends Application
 	        StackPane root1 = new StackPane();
 	        root1.getChildren().add(btn1);
 	        primaryStage.setScene(new Scene(root1, 300, 250));
+	        primaryStage.show();
+	       
+//Green Button
+	       primaryStage.setTitle("Green");
+	        Button btn2 = new Button();
+	        btn.setText("Say 'Green'");
+	        btn.setOnAction(new EventHandler<ActionEvent>() {
+	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	                System.out.println("Green");
+	            }
+	        });
+	        
+	        StackPane root2 = new StackPane();
+	        root2.getChildren().add(btn2);
+	        primaryStage.setScene(new Scene(root2, 300, 250));
+	        primaryStage.show();
+//Yellow Button
+	        primaryStage.setTitle("Yellow");
+	        Button btn3 = new Button();
+	        btn.setText("Say 'Yellow'");
+	        btn.setOnAction(new EventHandler<ActionEvent>() {
+	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	                System.out.println("Yellow");
+	            }
+	        });
+	        
+	        StackPane root3 = new StackPane();
+	        root3.getChildren().add(btn3);
+	        primaryStage.setScene(new Scene(root3, 300, 250));
 	        primaryStage.show();
 	}
 
