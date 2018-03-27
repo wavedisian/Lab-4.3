bc38df4185ebee763f2c9c407c039ad1013b91df
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2018-03-27 09:07:59 -0400

    Beylo

--- Lab 4.3/src/defaultPackage/GameCode.java
+++ Lab 4.3/src/defaultPackage/GameCode.java
@@ -8,6 +8,43 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class GameCode extends Application{
+public class GameCode extends Application
+{
+	@Override
+	public void start(Stage primaryStage) 
+	{
+		//Red Button
+		 primaryStage.setTitle("Red");
+	        Button btn = new Button();
+	        btn.setText("Say 'Red'");
+	        btn.setOnAction(new EventHandler<ActionEvent>() {
+	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	                System.out.println("Red");
+	            }
+	        });
+	        
+	        StackPane root = new StackPane();
+	        root.getChildren().add(btn);
+	        primaryStage.setScene(new Scene(root, 300, 250));
+	        primaryStage.show();
+	   	 //Blue Button
+	      primaryStage.setTitle("Blue");
+	        Button btn1 = new Button();
+	        btn.setText("Say 'Blue'");
+	        btn.setOnAction(new EventHandler<ActionEvent>() {
+	 
+	            @Override
+	            public void handle(ActionEvent event) {
+	                System.out.println("Blue");
+	            }
+	        });
+	        
+	        StackPane root1 = new StackPane();
+	        root1.getChildren().add(btn1);
+	        primaryStage.setScene(new Scene(root1, 300, 250));
+	        primaryStage.show();
+	}
 
 }
