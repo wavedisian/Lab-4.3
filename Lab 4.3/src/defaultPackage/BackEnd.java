102d83d043786c571f8e02732eedca20b91890a4
mrpresident2018 <sbansal3625@bths.edu>
2018-04-04 15:54:59 -0400

    Fixed some parts for the beginning code; but I don't know what is wrong because my laptop is weird.

--- Lab 4.3/src/defaultPackage/BackEnd.java
+++ Lab 4.3/src/defaultPackage/BackEnd.java
@@ -1,5 +1,66 @@
+//Wes Avedisian & Saurabh Bansal
+//Help from Steven Fong
 package defaultPackage;
 
-public class BackEnd {
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+
+public class BackEnd 
+{
+	public static List<String> CSVreader(String filename)
+	{
+		List<String> scores = new ArrayList<String>();
+		Path pathToFile = Paths.get(filename);
+		
+		try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII))
+		{
+			String line = br.readline();
+			
+			while(line != null)
+			{
+				String[] elements = line.split(",");
+				
+				for (String s: elements)
+				{
+					scores.add(s);
+				}
+				
+				line = br.readLine();
+			}
+		}
+		catch (IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+		return scores;
+	}
+	
+	public static void updateCSV(String filename, String replace)
+	{
+		try 
+		{
+			FileWriter f = new FileWriter(filename, false);
+			BufferedWriter b = new BufferedWriter(f);
+			PrintWriter p = new PrintWriter(b);
+			
+			p.print(replace);
+			p.flush();
+			p.close();
+		}
+		catch (IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+	}
 }
--- Lab 4.3/src/defaultPackage/GameCode.java
+++ Lab 4.3/src/defaultPackage/GameCode.java
@@ -1,83 +1,206 @@
+//Saurabh Bansal & Wes Avedisian
 package defaultPackage;
 
 import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.geometry.Pos;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
-public class GameCode extends Application
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+import javafx.scene.text.*;
+import javafx.scene.control.Label;
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.*;
+import javafx.animation.Timeline;
+import java.util.List;
+
+public class Gamecode extends Application 
 {
+
+	private static int score;
+	private boolean scoring = true;
+	private long timeStep;
+
+	private final Integer startTime = 30;
+	private Integer timeSeconds = startTime;
+	private Text time = new Text();
+	
+	private AnimationTimer timer;
+	
+
+	public static void main(String[] args) 
+	{
+		List<String> highscores = Backend.CSVreader("scores.txt");
+		score = Integer.parseInt(highscores.get(0));
+		launch(args);
+	}
+
 	@Override
 	public void start(Stage primaryStage) 
 	{
-//Red Button
-		 primaryStage.setTitle("Red");
-	        Button btn = new Button();
-	        btn.setText("Say 'Red'");
-	        btn.setOnAction(new EventHandler<ActionEvent>() {
-	 
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Red");
-	            }
-	        });
-	        
-	        StackPane root = new StackPane();
-	        root.getChildren().add(btn);
-	        primaryStage.setScene(new Scene(root, 300, 250));
-	        primaryStage.show();
-//Blue Button
-	      primaryStage.setTitle("Blue");
-	        Button btn1 = new Button();
-	        btn.setText("Say 'Blue'");
-	        btn.setOnAction(new EventHandler<ActionEvent>() {
-	 
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Blue");
-	            }
-	        });
-	        
-	        StackPane root1 = new StackPane();
-	        root1.getChildren().add(btn1);
-	        primaryStage.setScene(new Scene(root1, 300, 250));
-	        primaryStage.show();
-	       
-//Green Button
-	       primaryStage.setTitle("Green");
-	        Button btn2 = new Button();
-	        btn.setText("Say 'Green'");
-	        btn.setOnAction(new EventHandler<ActionEvent>() {
-	 
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Green");
-	            }
-	        });
-	        
-	        StackPane root2 = new StackPane();
-	        root2.getChildren().add(btn2);
-	        primaryStage.setScene(new Scene(root2, 300, 250));
-	        primaryStage.show();
-//Yellow Button
-	        primaryStage.setTitle("Yellow");
-	        Button btn3 = new Button();
-	        btn.setText("Say 'Yellow'");
-	        btn.setOnAction(new EventHandler<ActionEvent>() {
-	 
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Yellow");
-	            }
-	        });
-	        
-	        StackPane root3 = new StackPane();
-	        root3.getChildren().add(btn3);
-	        primaryStage.setScene(new Scene(root3, 300, 250));
-	        primaryStage.show();
-	}
+		
+		//StackPane root = new StackPane();
+		//GridPane root = new GridPane();
+		Pane root = new Pane();
+		
+		Button clicker = new Button();
+		Text txt = new Text(10, 0, "Click me");
+		
+		Button replay = new Button();
+		Button start = new Button();
+		root.getChildren().addAll(start);
+		
+		start.setText("Start Clicking!!");
+		start.setOnAction(new EventHandler<ActionEvent>() 
+				{
+			
+			@Override
+			public void handle(ActionEvent event) 
+			{
+				
+				timeStep = System.nanoTime() + 1000000000L;
+				timer = new AnimationTimer() 
+				{
+					
+					@Override
+					public void handle(long now) 
+					{
 
+						if(now>timeStep) 
+						{
+							timeStep = now + 1000000000L;
+							scoring = !scoring;
+						}
+						if(!scoring) 
+						{
+							clicker.setText("Dont't Click");
+						}
+						else 
+						{
+							clicker.setText("Click Me!");
+						}
+
+						txt.setText("Score: " + Integer.toString(score));
+					}
+				};
+				
+				Timeline timeline = new Timeline();
+				timeline.setCycleCount(Timeline.INDEFINITE);
+				
+				if (timeline!=null) 
+				{
+					timeline.stop();
+				}
+				
+				time.setText("Time Left: " + timeSeconds.toString());
+				
+				KeyFrame keyframe = new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>() 
+						{
+					@Override
+					public void handle(ActionEvent event) 
+					{
+						timeSeconds--;
+						time.setText("Time Left: " + timeSeconds.toString());
+						
+						if(timeSeconds<=0) 
+						{
+							timeline.stop();
+							
+							root.getChildren().removeAll(clicker,txt,time);
+							root.getChildren().add(replay);
+							timer.stop();
+						}
+					}
+				}
+				);
+				
+				timeline.getKeyFrames().add(keyframe);
+				
+				root.getChildren().remove(start);
+				root.getChildren().addAll(clicker,time,txt);
+				timeline.playFromStart();
+				timer.start();
+			}
+		});
+		
+		replay.setOnAction(new EventHandler<ActionEvent>() 
+				{
+			
+			@Override 
+			public void handle(ActionEvent event) 
+			{
+				
+				root.getChildren().remove(replay);
+				root.getChildren().addAll(clicker,txt,time);
+				
+				timeStep = System.nanoTime() + 1000000000L;
+				timer = new AnimationTimer() 
+				{
+					
+					@Override
+					public void handle(long now) 
+					{
+
+						if(now>timeStep) 
+						{
+							timeStep = now + 1000000000L;
+							scoring = !scoring;
+						}
+						if(!scoring) 
+						{
+							clicker.setText("Dont't Click");
+						}
+						else {
+							clicker.setText("Click Me!");
+						}
+
+						txt.setText("Score: " + Integer.toString(score));
+					}
+				
+				};
+				
+				timer.start();
+			}
+			
+		}
+		);
+			
+		clicker.relocate(230, 240);
+		txt.relocate(240, 100);
+		time.relocate(100, 100);
+		//startTimer();
+		
+		primaryStage.setTitle("Click");
+		primaryStage.setScene(new Scene(root, 500, 500));
+		primaryStage.show();
+
+		clicker.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) 
+			{
+				if(scoring) 
+				{
+					score++;
+				}
+
+				else 
+				{
+					score--;
+				}
+				
+				Backend.updateCSV("scores.txt", String.valueOf(score));
+			}
+		}
+		);
+	}
 }
